e6f9c49aab65eb54158a3d6c8a5b62bb69dc2077
James Hughes <james.a.hughes@gmail.com>
2022-04-06 21:05:04 -0500

    add hover animations, refactor css and html

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -5,11 +5,11 @@ import IconO from "../components/svgs/IconO";
 import IconX from "../components/svgs/IconX";
 
 const NewGame = () => {
-  const [playerOneMark, setPlayerOneMark] = useState("X");
+  const [playerOneMark, setPlayerOneMark] = useState("O");
 
   const handleClick = (mark) => {
-    console.log(mark)
-    setPlayerOneMark(mark)
+    console.log(mark);
+    setPlayerOneMark(mark);
   };
 
   return (
@@ -19,30 +19,34 @@ const NewGame = () => {
         <h4>PICK PLAYER 1'S MARK</h4>
         <div className="new-game__selector">
           <button
-            className={
-              playerOneMark === "X"
-                ? "module__select-button--active"
-                : "module__select-button"
-            }
+            className={`module__select-button ${
+              playerOneMark === "X" && "module__select-button--active"
+            }`}
             onClick={() => handleClick("X")}
           >
-            <IconX />
+            <IconX active={ playerOneMark === 'X' ? true : false } />
           </button>
           <button
-            className={
-              playerOneMark === "O"
-                ? "module__select-button--active"
-                : "module__select-button"
-            }
+            className={`module__select-button ${
+              playerOneMark === "O" && "module__select-button--active"
+            }`}
             onClick={() => handleClick("O")}
           >
-            <IconO />
+            <IconO active={ playerOneMark === 'O' ? true : false } />
           </button>
         </div>
         <p>REMEMBER : X GOES FIRST</p>
       </div>
-      <Button className="btn--yellow" text="NEW GAME (VS CPU)" />
-      <Button className="btn--blue" text="NEW GAME (VS PLAYER)" />
+      <div className="new-game__button-container">
+        <Button
+          className="btn--yellow new-game__type"
+          text="NEW GAME (VS CPU)"
+        />
+        <Button
+          className="btn--blue new-game__type"
+          text="NEW GAME (VS PLAYER)"
+        />
+      </div>
     </div>
   );
 };
