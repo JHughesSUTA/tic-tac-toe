ffb555608667f69481145cd370268dbf7b7a4ac8
James Hughes <james.a.hughes@gmail.com>
2022-04-07 20:54:16 -0500

    test new game screen

--- src/components/NewGameScreen/__test__/NewGameScreen.test.js
+++ src/components/NewGameScreen/__test__/NewGameScreen.test.js
@@ -1,5 +1,5 @@
 import NewGameScreen from "../NewGameScreen"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 
 describe("New Game Screen", () => {
   it("should change player 1's mark on selection", () => {
@@ -7,4 +7,23 @@ describe("New Game Screen", () => {
     const buttonXElement = screen.getByTestId("choice_x")
     expect(buttonXElement).toBeInTheDocument()
   })
+
+  it("should only highlight the selected player 1 mark", () => {
+    render(<NewGameScreen />)
+    const buttonXElement = screen.getByTestId("choice_x")
+    const buttonOElement = screen.getByTestId("choice_o");
+    fireEvent.click(buttonXElement)
+    expect(buttonXElement).toHaveClass("module__select-button--active");
+    expect(buttonOElement).not.toHaveClass("module__select-button--active");
+  })
+
+  it("should only highlight the selected player 1 mark after two clicks", () => {
+    render(<NewGameScreen />);
+    const buttonXElement = screen.getByTestId("choice_x");
+    const buttonOElement = screen.getByTestId("choice_o");
+    fireEvent.click(buttonXElement);
+    fireEvent.click(buttonOElement);
+    expect(buttonOElement).toHaveClass("module__select-button--active");
+    expect(buttonXElement).not.toHaveClass("module__select-button--active");
+  });
 })
